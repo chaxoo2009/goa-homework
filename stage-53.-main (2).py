c4314587f37d8e5985a0a8b4c098512684fc319d
chaxoo2009 <Chaxutamariami25@gmail.com>
2024-10-20 04:41:38 +0400

    first commit

---
+++ stage-53.-main (2).py
@@ -0,0 +1,29 @@
+#
+# # print("Mariam")
+# # print ("chakhutashvili")
+# # print ("15")
+# # print  (15)
+
+
+# print ("name"+ "surname" + "age"+ "hobby" + "favcar")
+# print ("Mariam" + "chakhutashvili" +"15" + " watching films and reading books about philosopy")
+ # print ("my favorite car is BMW")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
